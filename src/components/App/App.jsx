5b75d35663139a07babd40072f855f8ff5824cd9
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-05-26 15:44:49 -0500

    Add cork background to all routes and create not found route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<h1>This is the Home Page</h1>} />
-      <Route path="trash" element={<h1>Trash</h1>} />
-    </Routes>
+    <div className="bg-cork h-screen bg-cover">
+      <Routes>
+        <Route path="/" element={<h1>This is the Home Page</h1>} />
+        <Route path="trash" element={<h1>Trash</h1>} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </div>
   </BrowserRouter>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        cork: "url('./assets/images/cork-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
