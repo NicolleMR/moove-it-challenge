cf10f2b6ea39169ecfd3a92e4f22e628108e714d
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-06-04 11:19:56 -0500

    Markup and style of not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,9 @@
-const NotFound = () => <h1>Not Found</h1>;
+const NotFound = () => (
+  <div className="text-center">
+    <h1 className="inline-block bg-yellow-400 p-5	text-3xl font-bold text-white">
+      Sorry, the page you are trying to visit was not found
+    </h1>
+  </div>
+);
 
 export default NotFound;
