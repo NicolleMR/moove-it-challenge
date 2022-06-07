00a042affb2cc29a4557224c29fa59df0518b114
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-06-07 00:29:57 -0500

    Create notes state and update localStorage notes every time notes state change

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,32 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import NotFound from "../../pages/NotFound";
 import Trash from "../../pages/Trash";
 import Header from "../Header";
+import { getLocalStorageNotes, updateLocalStorageNotes } from "../../utils/localStorage";
 
-const App = () => (
-  <BrowserRouter>
-    <div className="flex min-h-screen flex-col items-center justify-center bg-cork bg-cover px-6 pt-24 pb-8">
-      <Header />
-      <div className="mx-auto w-full max-w-screen-xl px-2.5">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="trash" element={<Trash />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+const App = () => {
+  const [notes, setNotes] = useState(getLocalStorageNotes());
+
+  useEffect(() => {
+    updateLocalStorageNotes(notes);
+  }, [notes]);
+
+  return (
+    <BrowserRouter>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-cork bg-cover px-6 pt-24 pb-8">
+        <Header notes={notes} setNotes={setNotes} />
+        <div className="mx-auto w-full max-w-screen-xl px-2.5">
+          <Routes>
+            <Route path="/" element={<Home notes={notes} setNotes={setNotes} />} />
+            <Route path="trash" element={<Trash notes={notes} setNotes={setNotes} />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
-);
+    </BrowserRouter>
+  );
+};
 
 export default App;
